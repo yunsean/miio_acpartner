6aac489aba8eb147e3dd6748749d54a36b0d9a3f
Dylan <yunsean@163.com>
2018-08-14 22:22:25 +0800

    basic version

---
+++ miio_acpartner/xiaomi_miio.py
@@ -0,0 +1,433 @@
+"""
+Support for the Xiaomi Gateway Radio.
+
+For more details about this platform, please refer to the documentation
+https://home-assistant.io/components/radio.xiaomi_miio/
+"""
+import logging
+import time
+import json
+import os
+import urllib
+import requests
+import asyncio
+import async_timeout
+
+from datetime import timedelta
+from haffmpeg.core import HAFFmpeg
+
+import voluptuous as vol
+
+from homeassistant.components.miio_acpartner import (
+    PLATFORM_SCHEMA, DOMAIN, ATTR_VOLUME, RadioDevice)
+from homeassistant.components.ffmpeg import (
+    DATA_FFMPEG, CONF_EXTRA_ARGUMENTS)    
+from homeassistant.const import (
+    CONF_NAME, CONF_HOST, CONF_TOKEN, CONF_TIMEOUT,
+    ATTR_ENTITY_ID, ATTR_HIDDEN, CONF_COMMAND)
+from homeassistant.exceptions import PlatformNotReady
+import homeassistant.helpers.config_validation as cv
+from homeassistant.util.dt import utcnow
+
+REQUIREMENTS = ['python-miio==0.4.0', 'construct==2.9.41']
+DEPENDENCIES = ['ffmpeg']
+
+_LOGGER = logging.getLogger(__name__)
+CONF_APIKEY = 'api_key'
+CONF_SECRETKEY = 'secret_key'
+CONF_SPEED =  'speed'
+CONF_PITCH = 'pitch'
+CONF_VOLUME = 'volume'
+CONF_PERSON = 'person'
+CONF_BASEURL = "base_url"
+CONF_BASEPATH = "base_path"
+CONF_NOTIFY = "notify"
+
+LEARN_COMMAND_SCHEMA = vol.Schema({
+    vol.Required(ATTR_ENTITY_ID): vol.All(str),
+})
+
+COMMAND_SCHEMA = vol.Schema({
+    vol.Required(CONF_COMMAND): vol.All(cv.ensure_list, [cv.string])
+    })
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME): cv.string,
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_TOKEN): vol.All(str, vol.Length(min=32, max=32)),
+    vol.Optional(CONF_APIKEY): cv.string,
+    vol.Optional(CONF_SECRETKEY): cv.string,
+    vol.Optional(CONF_BASEURL, default = ''): cv.string,
+    vol.Optional(CONF_BASEPATH, default = ''): cv.string,
+    vol.Optional(CONF_SPEED, default = '5'): cv.string,
+    vol.Optional(CONF_PITCH, default = '5'): cv.string,
+    vol.Optional(CONF_VOLUME, default = '7'): cv.string,
+    vol.Optional(CONF_PERSON, default = '0'): cv.string,
+    vol.Optional(CONF_NOTIFY, default = False): cv.boolean,
+}, extra=vol.ALLOW_EXTRA)
+
+
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
+    from miio import Device, DeviceException
+
+    host = config.get(CONF_HOST)
+    token = config.get(CONF_TOKEN)
+    baseUrl = config.get(CONF_BASEURL)
+    basePath = config.get(CONF_BASEPATH)
+    apiKey = config.get(CONF_APIKEY) 
+    secretKey = config.get(CONF_SECRETKEY)  
+    speed = config.get(CONF_SPEED, 5)
+    pitch = config.get(CONF_PITCH, 5)  
+    volume = config.get(CONF_VOLUME, 5) 
+    person = config.get(CONF_PERSON, 0) 
+    notify = config.get(CONF_NOTIFY, False)
+    _LOGGER.info("Initializing with host %s (token %s...)", host, token[:5])
+    device = Device(host, token, lazy_discover=False)
+    try:
+        device_info = device.info()
+        model = device_info.model
+        unique_id = "{}-{}".format(model, device_info.mac_address)
+        _LOGGER.info("%s %s %s detected",
+                     model,
+                     device_info.firmware_version,
+                     device_info.hardware_version)
+    except DeviceException as ex:
+        _LOGGER.error("Device unavailable or token incorrect: %s", ex)
+        raise PlatformNotReady
+    friendly_name = config.get(CONF_NAME, "miio_miio_acpartner_" + host.replace('.', '_'))
+    xiaomi_miio_radio = XiaomiMiioRadio(friendly_name, device, unique_id, apiKey, secretKey, speed, pitch, volume, person, baseUrl, basePath, notify)
+    async_add_devices([xiaomi_miio_radio])
+
+TOKEN_INTERFACE = 'https://openapi.baidu.com/oauth/2.0/token'
+TEXT2AUDIO_INTERFACE = 'http://tsn.baidu.com/text2audio'
+class BaiduTTS:
+    def __init__(self, apiKey, secretKey, speed = 5, pitch = 5, volume = 15, person = 0):
+        self.apiKey = apiKey
+        self.secretKey = secretKey
+        self.speed = speed
+        self.pitch = pitch
+        self.volume = volume
+        self.person = person
+        token = self.get_token()
+        _LOGGER.info("token =====>" + token)
+        self.Token = token
+    def get_token(self):
+        try:
+            resp = requests.get(TOKEN_INTERFACE, verify=False, params={'grant_type': 'client_credentials',
+                                                         'client_id': self.apiKey,
+                                                         'client_secret':self.secretKey})
+            if resp.status_code != 200:
+                _LOGGER.error('Get ToKen Http Error status_code:%s' % resp.status_code)
+                return None
+            resp.encoding = 'utf-8'
+            tokenJson = resp.json()
+            if not 'access_token' in tokenJson:
+                _LOGGER.error('Get ToKen Json Error!')
+                return None
+            return tokenJson['access_token']
+        except Exception as ex:
+            _LOGGER.error(ex)
+            return None
+    def generate_tts(self, message, file):
+        try:
+            if self.Token == None:
+                self.Token = self.get_token()
+            if self.Token == None:
+                _LOGGER.error('get_tts_audio Self.ToKen is nil')
+                return False
+            resp = requests.get(TEXT2AUDIO_INTERFACE, verify=False, params={'tex': urllib.parse.quote(message),
+                                                             'lan': 'zh',
+                                                             'tok': self.Token,
+                                                             'ctp': '1',
+                                                             'aue': 3,
+                                                             'cuid': 'HomeAssistant',
+                                                             'spd': self.speed,
+                                                             'pit': self.pitch,
+                                                             'vol': self.volume,
+                                                             'per': self.person})
+            if resp.status_code == 500:
+                _LOGGER.error('Text2Audio Error:500 Not Support.')
+                return False
+            if resp.status_code == 501:
+                _LOGGER.error('Text2Audio Error:501 Params Error')
+                return False
+            if resp.status_code == 502:
+                _LOGGER.error('Text2Audio Error:502 TokenVerificationError.')
+                _LOGGER.info('Now Get Token!')
+                self.Token = self.get_token()
+                return self.generate_tts(message, file)
+            if resp.status_code == 503:
+                _LOGGER.error('Text2Audio Error:503 Composite Error.')
+                return False
+            if resp.status_code != 200:
+                _LOGGER.error('get_tts_audio Http Error status_code:%s' % resp.status_code)
+                return False
+            open(file, "wb").write(resp.content)
+            return True    
+        except Exception as ex:
+            _LOGGER.error(ex)
+            return False
+
+class AacConverter(HAFFmpeg):
+    @asyncio.coroutine
+    def convert(self, input_source, output, extra_cmd=None, timeout=15):
+        command = [
+            "-vn",
+            "-c:a",
+            "aac",
+            "-strict",
+            "-2",
+            "-b:a",
+            "64K",
+            "-y"
+        ]         
+        is_open = yield from self.open(cmd=command, input_source=input_source, output=output, extra_cmd=extra_cmd)         
+        if not is_open:
+            _LOGGER.warning("Error starting FFmpeg.")
+            return False
+        try:
+            with async_timeout.timeout(timeout, loop=self._loop):
+                out, error = yield from self._proc.communicate() 
+        except (asyncio.TimeoutError, ValueError):
+            _LOGGER.error("Timeout convert audio file.")
+            self._proc.kill()
+            return False    
+        return True         
+            
+class XiaomiMiioRadio(RadioDevice):
+    def __init__(self, friendly_name, device, unique_id, apiKey, secretKey, speed, pitch, volume, person, baseUrl, basePath, notify):
+        self._name = friendly_name
+        self._device = device
+        self._unique_id = unique_id
+        self._state = False
+        self._baidu = None
+        self._apiKey = apiKey
+        self._secretKey = secretKey
+        self._speed = speed
+        self._pitch = pitch
+        self._volume = volume
+        self._person = person
+        self._mp3Path = basePath + "/www/tts_" + str(unique_id).replace(":", "") + ".mp3"
+        self._aacPath = basePath + "/www/tts_" + str(unique_id).replace(":", "") + ".aac"
+        self._ttsUrl = baseUrl + "/local/tts_" + str(unique_id).replace(":", "") + ".aac"
+        self._notify = notify
+    @property
+    def name(self):
+        return self._name
+    @property
+    def device(self):
+        return self._device
+    @property
+    def is_on(self):
+        from miio import DeviceException
+        try:
+            status = self._device.send("get_prop_fm", [])
+            return status["current_status"] == "run"
+        except DeviceException:
+            return False
+    @property
+    def should_poll(self):
+        return True
+    @property
+    def device_state_attributes(self):
+        status = self._device.send("get_prop_fm", [])
+        attrs = {
+            'hidden': 'true',
+            'channels': self._device.send("get_channels", {"start": 0}),
+            'space_free': self._device.send("get_music_free_space", []),
+            'channel': status["current_program"],
+            'volume': status["current_volume"],
+            'ringtones': {
+                'alarm': self._device.send("get_music_info", [0]),
+                'clock': self._device.send("get_music_info", [1]),
+                'doorbell': self._device.send("get_music_info", [2])
+            }
+        }
+        return attrs      
+        
+    @asyncio.coroutine
+    def async_toggle(self, **kwargs):
+        if self.is_on:
+            self._device.send('play_fm', ["off"])
+        else:
+            self._device.send('play_fm', ["on"])
+    # pylint: disable=R0201
+    @asyncio.coroutine
+    def async_turn_on(self, **kwargs):
+        self._device.send('play_fm', ["on"])
+    @asyncio.coroutine
+    def async_turn_off(self, **kwargs):
+        self._device.send('play_fm', ["off"])
+                    
+    def set_volume(self, volume, **kwargs):
+        if volume is None:
+            _LOGGER.debug("Empty packet.")
+            return True
+        try:
+            self._device.send('volume_ctrl_fm', [str(volume)])
+        except ValueError as error:
+            _LOGGER.error(error)
+            return False
+        return True  
+    def next_radio(self, **kwargs):
+        try:
+            status = self._device.send("get_prop_fm", [])
+            channel = status["current_program"]
+            channels = self._device.send("get_channels", {"start": 0})
+            chs = channels["chs"]
+            if len(chs) < 1:
+                return False
+            current_index = -1
+            for idx, val in enumerate(chs):
+                if val["id"] == channel:
+                    current_index = idx
+                    break
+            if current_index == -1:
+                current_index = 0
+            elif current_index >= len(chs) - 1:
+                current_index = 0
+            else:
+                current_index = current_index + 1
+            channel = chs[current_index]
+            self._device.send("play_specify_fm", {'id': channel["id"], 'type': 0})
+        except ValueError as error:
+            _LOGGER.error(error)
+            return False
+        return True
+    def prev_radio(self, **kwargs):
+        try:
+            status = self._device.send("get_prop_fm", [])
+            channel = status["current_program"]
+            channels = self._device.send("get_channels", {"start": 0})
+            chs = channels["chs"]
+            if len(chs) < 1:
+                return False
+            current_index = -1
+            for idx, val in enumerate(chs):
+                if val["id"] == channel:
+                    current_index = idx
+                    break
+            if current_index == -1:
+                current_index = 0
+            elif current_index == 0:
+                current_index = len(chs) - 1
+            else:
+                current_index = current_index - 1
+            channel = chs[current_index]
+            self._device.send("play_specify_fm", {'id': channel["id"], 'type': 0})
+        except ValueError as error:
+            _LOGGER.error(error)
+            return False
+        return True  
+
+    def play_radio(self, program_id, **kwargs):
+        if program_id is None:
+            return True
+        try:
+            self._device.send('play_specify_fm', {'id': int(str(program_id)), 'type': 0})
+        except ValueError as error:
+            _LOGGER.error(error)
+            return False
+        return True
+    @asyncio.coroutine
+    def async_play_ringtone(self, ringtone_id, **kwargs):
+        if ringtone_id is None:
+            return True
+        volume = kwargs.get(ATTR_VOLUME)
+        try:
+            if volume == None:
+                self._device.send('play_music',[int(str(ringtone_id))])
+            else:    
+                self._device.send('play_music_new', [str(ringtone_id), int(str(volume))])
+        except ValueError as error:
+            _LOGGER.error(error)
+            return False
+        return True
+       
+    @asyncio.coroutine
+    def async_play_tts(self, message, **kwargs):
+        if message is None:
+            _LOGGER.warning("message is not present.")
+            return True
+        volume = kwargs.get(ATTR_VOLUME)        
+        try:
+            if (self._baidu == None) and (self._apiKey == None or self._secretKey == None):
+                _LOGGER.error("The baidu tts api key is not configured.")
+                return False
+            if self._baidu == None:
+                self._baidu = BaiduTTS(self._apiKey, self._secretKey, self._speed, self._pitch, self._volume, self._person)
+            if not self._baidu.generate_tts(message, self._mp3Path):
+                _LOGGER.error("generate tts failed.")
+                return False
+            if os.path.exists(self._aacPath):
+                os.remove(self._aacPath)
+            ffmpeg = AacConverter(self.hass.data[DATA_FFMPEG].binary, loop=self.hass.loop)
+            result = yield from ffmpeg.convert(self._mp3Path, output=self._aacPath)
+            if (not result) or (not os.path.exists(self._aacPath)) or (os.path.getsize(self._aacPath) < 1):
+                _LOGGER.error("Convert file to aac failed.")
+                return False 
+            self._device.send("download_user_music", ["99999", self._ttsUrl])
+            index = 0
+            while index < 10:
+                progess = self._device.send("get_download_progress", [])
+                if str(progess) == "['99999:100']":
+                    break
+                index += 1
+                yield from asyncio.sleep(1)
+            if (index >= 10):
+                _LOGGER.error("download tts file to gateway failed.")
+                return False
+            if volume == None:
+                self._device.send('play_music', [99999])
+            else:    
+                self._device.send('play_music_new', ["99999", int(str(volume))])
+            _LOGGER.error(message) 
+            if self._notify:
+                log_msg = "TTS: %s" % message
+                self.hass.components.persistent_notification.async_create(log_msg, title='AC partner TTS', notification_id=99999) 
+        except Exception as error:
+            _LOGGER.error(error)
+            return False
+        return True 
+       
+    @asyncio.coroutine
+    def async_play_vod(self, url, **kwargs):
+        if url is None:
+            _LOGGER.warning("message is not present.")
+            return True
+        volume = kwargs.get(ATTR_VOLUME)        
+        try:
+            urllib.request.urlretrieve(url, self._mp3Path)
+            if os.path.exists(self._aacPath):
+                os.remove(self._aacPath)
+            ffmpeg = AacConverter(self.hass.data[DATA_FFMPEG].binary, loop=self.hass.loop)
+            result = yield from ffmpeg.convert(self._mp3Path, output=self._aacPath)
+            if (not result) or (not os.path.exists(self._aacPath)) or (os.path.getsize(self._aacPath) < 1):
+                _LOGGER.error("Convert file to aac failed.")
+                return False 
+            self._device.send("download_user_music", ["99999", self._ttsUrl])
+            index = 0
+            while index < 60:
+                progess = self._device.send("get_download_progress", [])
+                _LOGGER.error(progess)
+                if str(progess) == "['99999:100']":
+                    break
+                index += 1
+                yield from asyncio.sleep(1)
+            if (index >= 60):
+                _LOGGER.error("download tts file to gateway failed.")
+                return False
+            if volume == None:
+                self._device.send('play_music', [99999])
+            else:    
+                self._device.send('play_music_new', ["99999", int(str(volume))])
+            _LOGGER.error("play_vod(%s)" % url) 
+            if self._notify:
+                log_msg = "VOD finished."
+                self.hass.components.persistent_notification.async_create(log_msg, title='AC partner TTS', notification_id=99999) 
+        except Exception as error:
+            _LOGGER.error(error)
+            return False
+        return True  
+            
